6b7da6b9688687fb5447eb3b7c1b96261b6e73e4
Philippe <kamokamophilippe13@gmail.com>
2022-08-09 22:49:35 -0500

    css

--- src/components/TemplateClass/TemplateClass.jsx
+++ src/components/TemplateClass/TemplateClass.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
