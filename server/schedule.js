fd69ca2708c62aa454adc8d76870999a4a41f9bd
Philippe <kamokamophilippe13@gmail.com>
2022-12-23 21:28:22 -0600

    comment

--- server/schedule.js
+++ server/schedule.js
@@ -1,25 +1,34 @@
-const pool = require('./modules/pool');
-require("dotenv").config();
+// const pool = require('./modules/pool');
+// require("dotenv").config();
 
 
-function scheduleDelete(){
-    //delete from "event" where DATE_TRUNC('DAY', date) < CURRENT_DATE + INTERVAL '2days';
-    console.log("running schedule delete");
-    const queryText = `delete from "new" where DATE_TRUNC('DAY', date) = CURRENT_DATE;`
-    const queryText1 =`delete from "event" where DATE_TRUNC('DAY', date) = CURRENT_DATE;`
-    pool.query(queryText).then(() => {
-        console.log("new was delete")
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    pool.query(queryText1).then(()=> {
-        console.log(" event deleve event")
-    })
-    .catch((error) =>{
-        console.log(error);
-    })
+// function scheduleDelete(){
+//     //delete from "event" where DATE_TRUNC('DAY', date) < CURRENT_DATE + INTERVAL '2days';
+//     console.log("running schedule delete");
+//     const queryText = `delete from "new" where DATE_TRUNC('DAY', date) <= CURRENT_DATE;`
+//     const queryText1 =`delete from "event" where DATE_TRUNC('DAY', date) <= CURRENT_DATE;`
+//     pool.query(queryText).then(() => {
+//         console.log("new was delete")
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+//     pool.query(queryText1).then(()=> {
+//         console.log(" event deleve event")
+//     })
+//     .catch((error) =>{
+//         console.log(error);
+//     })
 
+// }
+
+// module.exports =scheduleDelete;
+
+
+
+var counter = 0 ;
+while (counter <= 10) {
+    console.log("Hi")
+    counter +=1;
 }
-
-module.exports =scheduleDelete;
+console.log("bye");
